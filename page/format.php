254e01df59dacb631aa4b46338a8eaf4b4d1f1ac
pascal fautrero <pascal.fautrero@crdp.ac-versailles.fr>
2013-09-22 17:03:10 +0200

    fix MAIN_DIR value

--- page/format.php
+++ page/format.php
@@ -25,11 +25,7 @@
  */
 defined('MOODLE_INTERNAL') || die();
 global $LOCAL_PATH, $MAIN_DIR, $CFG;
-/** $MAIN_DIR = "page" * */
-// test
-$MAIN_DIR = substr(
-    dirname(__FILE__), strpos(dirname(__FILE__), "course/format") + 14
-);
+$MAIN_DIR = "page";
 $LOCAL_PATH = ".";
 if (
     file_exists("./format/" . $MAIN_DIR 
